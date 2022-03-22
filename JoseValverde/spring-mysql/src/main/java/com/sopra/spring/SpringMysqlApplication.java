e75c3b4a45c71b6c7a7a894b0c8c80c598ce4b0b
Jose Valverde <jovallo@gmail.com>
2022-03-22 12:05:33 +0100

    subida de codigo

--- JoseValverde/spring-mysql/src/main/java/com/sopra/spring/SpringMysqlApplication.java
+++ JoseValverde/spring-mysql/src/main/java/com/sopra/spring/SpringMysqlApplication.java
@@ -15,18 +15,18 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class SpringMysqlApplication {
 	
-	@Autowired
-	PersonaRepository personaRepository;
+//	@Autowired
+//	PersonaRepository personaRepository;
 
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringMysqlApplication.class, args);
 	}
 	
-	@PostConstruct
-	public void init() {
-		Persona p = personaRepository.save(new Persona(null, "Persona1", 30));
-		log.info("Persona: {}", p);
-	}
+////	@PostConstruct
+//	public void init() {
+//		Persona p = personaRepository.save(new Persona(null, "Persona1", 30));
+//		log.info("Persona: {}", p);
+//	}
 
 }
