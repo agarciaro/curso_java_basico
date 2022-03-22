494cea50f64e9b083b31920f63ce7dba1e641c51
agarciaro <aitorgarciarojado@gmail.com>
2022-03-22 12:05:14 +0100

    RestController

---
+++ spring-mysql/src/main/java/com/sopra/spring/service/PersonaServiceImpl.java
@@ -0,0 +1,21 @@
+package com.sopra.spring.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sopra.spring.model.Persona;
+import com.sopra.spring.repository.PersonaRepository;
+
+@Service
+public class PersonaServiceImpl implements PersonaService {
+	
+	@Autowired
+	PersonaRepository personaRepository;
+	
+	@Override
+	public Persona insert(Persona p) {
+		
+		return personaRepository.save(p);
+	}
+
+}
