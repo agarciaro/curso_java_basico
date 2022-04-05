2ddb0bc983cef4c09e5cd60eb6832f0982c3e9a8
Cristian Juan <cjuaniestacio@gmail.com>
2022-04-05 17:01:13 +0200

    CRUD Peliculas

---
+++ PRACTICA/Equipo4/videoclub-backend/peliculas-service/src/main/java/com/sopra/equipo4/controller/PeliculaRestController.java
@@ -0,0 +1,51 @@
+package com.sopra.equipo4.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sopra.equipo4.model.Pelicula;
+import com.sopra.equipo4.service.PeliculaService;
+
+@RestController
+@RequestMapping("/api")
+public class PeliculaRestController {
+
+	@Autowired
+	PeliculaService peliculaService;
+	
+	@GetMapping("/peliculas")
+	public List<Pelicula> getAllPeliculas() {
+		return peliculaService.getAllPeliculas();
+	}
+	
+	@GetMapping("/peliculas/{id}")
+	public Pelicula getPeliculaById(@PathVariable Integer id) {
+		return peliculaService.getPeliculaById(id);
+	}
+	
+	@PostMapping("/peliculas")
+	public Pelicula insert(@RequestBody Pelicula pelicula) {
+		return peliculaService.insert(pelicula);
+	}
+	
+	@PutMapping("/peliculas")
+	public Pelicula update(@RequestBody Pelicula pelicula) {
+		return peliculaService.update(pelicula);
+	}
+	
+	@DeleteMapping("/peliculas/{id}")
+	public String delete(@PathVariable Integer id) {
+		return peliculaService.delete(id) ? "Se ha eliminado con éxito" : "Ha ocurrido un error al eliminar.";
+	}
+	
+	
+}
---
+++ PRACTICA/Equipo4/videoclub-backend/peliculas-service/src/main/java/com/sopra/equipo4/service/PeliculaServiceImpl.java
@@ -0,0 +1,44 @@
+package com.sopra.equipo4.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sopra.equipo4.model.Pelicula;
+import com.sopra.equipo4.repository.PeliculaRepository;
+
+@Service
+public class PeliculaServiceImpl implements PeliculaService {
+	
+	@Autowired
+	PeliculaRepository  peliculaRepository;
+
+	@Override
+	public List<Pelicula> getAllPeliculas() {
+		return (List<Pelicula>) peliculaRepository.findAll();
+	}
+
+	@Override
+	public Pelicula getPeliculaById(Integer id) {
+		return peliculaRepository.findById(id).get();
+	}
+
+	@Override
+	public Pelicula insert(Pelicula pelicula) {
+		return peliculaRepository.save(pelicula);
+	}
+
+	@Override
+	public Pelicula update(Pelicula pelicula) {
+		return peliculaRepository.save(pelicula);
+	}
+
+	@Override
+	public boolean delete(Integer id) {
+		peliculaRepository.deleteById(id);
+		
+		return peliculaRepository.findById(id).isEmpty() ? true : false;
+	}
+
+}
