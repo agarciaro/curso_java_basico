317c422f7b69f5c61b6401f5dced0b2aecaeda41
MiguelSierra <miguel.sierra.s@gmail.com>
2022-04-05 16:13:42 +0200

    ahora_si

---
+++ PRACTICA/Equipo1/VideoclubBackend/database-service/src/main/java/com/sopra/videoclub/repository/ActorRepository.java
@@ -0,0 +1,11 @@
+package com.sopra.videoclub.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.sopra.videoclub.model.Actor;
+
+//@Repository
+public interface ActorRepository{
+
+}
