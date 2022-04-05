a4518d5d3013517a2ead03ae88ae19c90e4bef7a
Valentin Hristov <valentinfyq@gmail.com>
2022-04-05 16:42:12 +0200

    ApiRest Socios

--- PRACTICA/Equipo4/videoclub-backend/database-service/src/main/java/com/sopra/equipo4/model/Pelicula.java
+++ PRACTICA/Equipo4/videoclub-backend/database-service/src/main/java/com/sopra/equipo4/model/Pelicula.java
@@ -10,7 +10,6 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
 import lombok.AllArgsConstructor;
---
+++ PRACTICA/Equipo4/videoclub-backend/socios-service/src/main/java/com/sopra/equipo4/service/SocioServiceImpl.java
@@ -0,0 +1,41 @@
+package com.sopra.equipo4.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sopra.equipo4.model.Socio;
+import com.sopra.equipo4.repository.SocioRepository;
+@Service
+public class SocioServiceImpl implements SocioService {
+	
+	@Autowired
+	SocioRepository socioRepository;
+
+	@Override
+	public List<Socio> findAll() {
+		return (List<Socio>) socioRepository.findAll();
+	}
+
+	@Override
+	public Socio findById(Integer id) {
+		return socioRepository.findById(id).get();
+	}
+
+	@Override
+	public void deleteSocioById(Integer id) {
+		socioRepository.deleteById(id);
+	}
+
+	@Override
+	public Socio updateSocio(Socio socio) {
+		return socioRepository.save(socio);
+	}
+
+	@Override
+	public Socio insertSocio(Socio socio) {
+		return socioRepository.save(socio);
+	}
+
+}
