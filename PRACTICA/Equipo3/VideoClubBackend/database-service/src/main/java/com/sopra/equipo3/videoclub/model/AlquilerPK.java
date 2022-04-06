fd494be2687713e91c1b23bd47a96503dbc3be51
Sergio Dung <90632325+Pewinfire@users.noreply.github.com>
2022-04-06 10:18:32 +0200

    correccion modelo bd equipo 3

--- PRACTICA/Equipo3/VideoClubBackend/database-service/src/main/java/com/sopra/equipo3/videoclub/model/AlquilerPK.java
+++ PRACTICA/Equipo3/VideoClubBackend/database-service/src/main/java/com/sopra/equipo3/videoclub/model/AlquilerPK.java
@@ -1,6 +1,7 @@
 package com.sopra.equipo3.videoclub.model;
 
 import java.io.Serializable;
+import java.time.LocalDate;
 
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
@@ -13,13 +14,16 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Embeddable
-public class AlquilerPK implements Serializable {
+public class AlquilerPK implements Serializable{
 
-	private static final long serialVersionUID = 4496720059051927881L;
 
+	private static final long serialVersionUID = 8887869270479460615L;
+	
 	@Column(name = "id_socio")
 	private Long idSocio;
 	@Column(name = "id_ejemplar")
 	private Long idEjemplar;
+	@Column(name = "fecha_inicio")
+	private LocalDate fechaInicio;
 
 }
