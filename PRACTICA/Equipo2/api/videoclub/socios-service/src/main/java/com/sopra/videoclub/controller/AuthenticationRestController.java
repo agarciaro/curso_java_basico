bbf45fdccfd63ec995e988f242da91b9353f8eec
Alberto <albertorodriguezsorli@gmail.com>
2022-04-07 11:35:04 +0200

    feat(#7) Acabando login

--- PRACTICA/Equipo2/api/videoclub/socios-service/src/main/java/com/sopra/videoclub/controller/AuthenticationRestController.java
+++ PRACTICA/Equipo2/api/videoclub/socios-service/src/main/java/com/sopra/videoclub/controller/AuthenticationRestController.java
@@ -3,6 +3,7 @@ package com.sopra.videoclub.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,50 +13,52 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.sopra.model.JwtToken;
-import com.sopra.model.UsuarioDetails;
+import com.sopra.videoclub.model.JwtToken;
 import com.sopra.videoclub.model.UsuarioCredenciales;
 import com.sopra.videoclub.service.JwtTokenUtil;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
-@RequestMapping("api/auth")
+@RequestMapping("/api/auth")
+@Slf4j
 public class AuthenticationRestController {
-//
-////	@Autowired
-////	AuthenticationManager authenticationManager;
-//
-//	@Autowired
-//	JwtTokenUtil jwtTokenUtil;
-//
-//	@Autowired
-//	UserDetailsService userDetailsService;
-//
-//	@Autowired
-//	PasswordEncoder encoder;
-//
-//	@Autowired
-//	JwtToken jwtToken;
-//
-//	@PostMapping("/logging")
-//	public JwtToken login(@RequestBody UsuarioCredenciales credenciales) {
-//		authenticationManager.authenticate(
-//				new UsernamePasswordAuthenticationToken(credenciales.getUsername(), credenciales.getPassword()));
-//		UsuarioDetails usuarioDetails = (UsuarioDetails) userDetailsService
-//				.loadUserByUsername(credenciales.getUsername());	
-//		String token = jwtTokenUtil.generateToken(usuarioDetails);
-//		
-//		return new JwtToken(token);
-//	}
-//	
-//	@GetMapping("/password/{password}/encode")
-//	public String encodePassword(@PathVariable String password) {
-//		return encoder.encode(password);
-//	}
-//	
-//	@GetMapping("/token/refresh")
-//	public JwtToken refreshToken() {
-//		String username = jwtTokenUtil.getUsuarioFromToken(jwtToken.getToken());
-//		UsuarioDetails usuarioDetails = (UsuarioDetails) userDetailsService.loadUserByUsername(username);
-//		return new JwtToken(jwtTokenUtil.generateToken(usuarioDetails));
-//	}
+
+	@Autowired
+	AuthenticationManager authenticationManager;
+
+	@Autowired
+	JwtTokenUtil jwtTokenUtil;
+
+	@Autowired
+	UserDetailsService userDetailsService;
+
+	@Autowired
+	PasswordEncoder encoder;
+
+	@Autowired
+	JwtToken jwtToken;
+
+	@PostMapping("/login")
+	public JwtToken login(@RequestBody UsuarioCredenciales credencials) {
+
+		UserDetails userDetails = userDetailsService.loadUserByUsername(credencials.getUsername());
+
+		String token = jwtTokenUtil.generateToken(userDetails);
+
+		return new JwtToken(token);
+	}
+
+	@GetMapping("/password/{password}/encode")
+	public String encodePassword(@PathVariable String password) {
+		return encoder.encode(password);
+	}
+
+	@GetMapping("/token/refresh")
+	public JwtToken refreshToken() {
+		String username = jwtTokenUtil.getUsuarioFromToken(jwtToken.getToken());
+		UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+		return new JwtToken(jwtTokenUtil.generateToken(userDetails));
+	}
+
 }
