a7f93cc4e21be42a09c1312672f28d2a6b68478a
Cesc Fe <76252400+FrancescFe@users.noreply.github.com>
2022-04-07 15:47:06 +0200

    init front vue

---
+++ PRACTICA/Equipo1/VideoclubFrontend/vue/videoclub-equipo1/src/components/MyComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="my-component">
+      <hr/>
+    <h1>This is my component</h1>
+    <h2>{{ subtitle }}</h2>
+    <hr/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MyComponent",
+  data() {
+    return {
+      subtitle: "I am a subtitle of my component",
+    };
+  },
+};
+</script>
