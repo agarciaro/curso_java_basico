b2be707e0b0dae0eb88f0b3a82c42668e89a4e10
jovalloSopra <jovallo@gmail.com>
2022-04-08 11:40:31 +0200

    Comentar codigo

--- PRACTICA/Equipo3/VideoClubBackend/videoclub-service/src/main/java/com/sopra/equipo3/videoclub/controller/ActorRestController.java
+++ PRACTICA/Equipo3/VideoClubBackend/videoclub-service/src/main/java/com/sopra/equipo3/videoclub/controller/ActorRestController.java
@@ -15,37 +15,50 @@ import org.springframework.web.bind.annotation.RestController;
 import com.sopra.equipo3.videoclub.model.entity.Actor;
 import com.sopra.equipo3.videoclub.service.ActorService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+
 @RestController
 @RequestMapping("/api")
 public class ActorRestController {
 	@Autowired
 	ActorService actorService;
 
+	@Operation(summary = "Retorna todos los actores registrados")
 	@GetMapping("/actor")
 	public List<Actor> getAllDirectors() {
 		return actorService.findAll();
 	}
 
+	@Operation(summary = "Retorna todos los actores registrados dado un codigo de actor valido")
+	@Parameters({@Parameter(name = "id", required = true, description = "El codigo de actor")})
 	@GetMapping("/actor/{id}")
 	public Actor getDirector(@PathVariable Long id) {
 		return actorService.findById(id);
 	}
-
+	
+	@Operation(summary = "Inserta los datos en la BBDD de actor")
 	@PostMapping("/actor")
 	public Actor insertActor(@RequestBody Actor actor) {
 		return actorService.insert(actor);
 	}
 
+	@Operation(summary = "Actualiza los datos en la BBDD de director")
 	@PutMapping("/actor")
 	public Actor updateDirector(@RequestBody Actor actor) {
 		return actorService.update(actor);
 	}
 
+	@Operation(summary = "Elimina los datos en la BBDD de actor dado un codigo de actor valido")
+	@Parameters({@Parameter(name = "id", required = true, description = "El codigo de actor")})
 	@DeleteMapping("/actor/{id}")
 	public void deleteActor(@PathVariable Long id) {
 		actorService.delete(id);
 	}
 	
+	@Operation(summary = "Retorna todos los actores que intervienen en la pelicula dado un codigo de pelicula valido")
+	@Parameters({@Parameter(name = "id", required = true, description = "El codigo de pelicula")})
 	@GetMapping("/pelicula/{id}/actor")
 	public List<Actor> getAllActoresByPelicula(@PathVariable Long id) {
 		return actorService.findActoresByPelicula(id);
--- PRACTICA/Equipo3/VideoClubBackend/videoclub-service/src/main/java/com/sopra/equipo3/videoclub/controller/DirectorRestController.java
+++ PRACTICA/Equipo3/VideoClubBackend/videoclub-service/src/main/java/com/sopra/equipo3/videoclub/controller/DirectorRestController.java
@@ -13,8 +13,15 @@ import org.springframework.web.bind.annotation.RestController;
 import com.sopra.equipo3.videoclub.model.entity.Director;
 import com.sopra.equipo3.videoclub.service.DirectorService;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.info.Info;
+
 import java.util.List;
 
+@OpenAPIDefinition(info = @Info(description="Gestion de las API del videoclub"))
 @RestController
 @RequestMapping("/api/director")
 public class DirectorRestController {
@@ -22,26 +29,33 @@ public class DirectorRestController {
 	@Autowired
 	DirectorService directorService;
 
+	@Operation(summary = "Retorna todos los directores registrados")
 	@GetMapping()
 	public List<Director> getAllDirectors() {
 		return directorService.findAll();
 	}
 
+	@Operation(summary = "Retorna todos los directores registrados dado un codigo de director valido")
+	@Parameters({@Parameter(name = "id", required = true, description = "El codigo de director")})
 	@GetMapping("/{id}")
 	public Director getDirector(@PathVariable Long id) {
 		return directorService.findById(id);
 	}
 
+	@Operation(summary = "Inserta los datos en la BBDD de director")
 	@PostMapping()
 	public Director insertDirector(@RequestBody Director director) {
 		return directorService.insert(director);
 	}
 
+	@Operation(summary = "Actualiza los datos en la BBDD de director")
 	@PutMapping()
 	public Director updateDirector(@RequestBody Director director) {
 		return directorService.update(director);
 	}
 
+	@Operation(summary = "Elimina los datos en la BBDD de director dado un codigo de director valido")
+	@Parameters({@Parameter(name = "id", required = true, description = "El codigo de director")})
 	@DeleteMapping("/{id}")
 	public void deleteDirector(@PathVariable Long id) {
 		directorService.delete(id);
